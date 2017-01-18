640c5b0c77eacb6e26df837f037c1baa76ca2ac2
Stephan Georg <s.georg@nearest.place>
2017-01-18 17:14:20 +0100

    Options as object

--- test/staticmaps.js
+++ test/staticmaps.js
@@ -1,30 +1,66 @@
 var should = require('should');
 var StaticMaps = require('../lib/staticmaps');
 
+var markerPath = __dirname + '/marker.png';
+
 
 describe('StaticMap', function(){
   var geocoder;
 
-  describe('Initializating', function() {
-
-
+  describe('Initializing ...', function() {
 
     it('without any arguments', function() {
 
       (function() {
-        map = new StaticMaps(500, 500);
+
+        var options = {
+          width: 600,
+          height: 400
+        };
+        map = new StaticMaps(options);
+
       }).should.not.throw();
     });
 
+  });
 
-    it('render', function(done) {
 
-      this.timeout(5000);
+  describe('Rendering ...', function() {
 
-      map = new StaticMaps(1200, 800);
+    it('render w/ center', function(done) {
+
+      this.timeout(3000);
+
+      var options = {
+        width: 600,
+        height: 400
+      };
+
+      map = new StaticMaps(options);
       map.render(13, [13.437524,52.4945528])
         .then(function(values) {
-          var save = map.image.save( 'simple.png');
+          var save = map.image.save( 'center.png');
+          save.on('finish', function () {
+            done();
+          });
+        })
+        .catch(function(err) { console.log(err); });
+
+    });
+
+    it('render w/ bbox', function(done) {
+
+      this.timeout(3000);
+
+      var options = {
+        width: 1200,
+        height: 800
+      };
+
+      map = new StaticMaps(options);
+      map.render(null, [11.414795,51.835778,11.645164,51.733833])
+        .then(function(values) {
+          var save = map.image.save('bbox.png');
           save.on('finish', function () {
             done();
           });
@@ -35,13 +71,28 @@ describe('StaticMap', function(){
 
     it('render w/ icon', function(done) {
 
-     this.timeout(10000);
+     this.timeout(3000);
 
-      var file = __dirname + '/marker.png';
+      var options = {
+        width: 500,
+        height: 500
+      };
 
-      map = new StaticMaps(500, 500);
-      map.addMarker([13.437524,52.4945528], file, 24, 48, 48, 48);
-      map.addMarker([13.430524,52.4995528], file, 24, 48, 48, 48);
+      map = new StaticMaps(options);
+
+      var marker = {
+        filePath: markerPath,
+        offset_x: 24,
+        offset_y: 48,
+        width: 48,
+        height: 48
+      };
+
+      marker.coord = [13.437524,52.4945528];
+      map.addMarker(marker);
+
+      marker.coord = [13.430524,52.4995528];
+      map.addMarker(marker);
 
       map.render(12, [13.437524,52.4945528])
         .then(function(values) {
@@ -56,14 +107,28 @@ describe('StaticMap', function(){
 
     it('render w/out center', function(done) {
 
-     this.timeout(10000);
+     this.timeout(3000);
 
-      var file = __dirname + '/marker.png';
+      var options = {
+        width: 1200,
+        height: 800
+      };
+      map = new StaticMaps(options);
 
-      map = new StaticMaps(1000, 1000);
-      map.addMarker([13.437524,52.4945528], file, 24, 48, 48, 48);
-      map.addMarker([13.430524,52.4995528], file, 24, 48, 48, 48);
-      map.addMarker([13.410524,52.5195528], file, 24, 48, 48, 48);
+      var marker = {
+        filePath: markerPath,
+        offset_x: 24,
+        offset_y: 48,
+        width: 48,
+        height: 48
+      };
+
+      marker.coord = [13.437524,52.4945528];
+      map.addMarker(marker);
+      marker.coord = [13.430524,52.4995528];
+      map.addMarker(marker);
+      marker.coord = [13.410524,52.5195528];
+      map.addMarker(marker);
 
       map.render()
         .then(function(values) {
