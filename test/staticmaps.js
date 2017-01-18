7f9ba4027032a8d06e44bac031a893d0e2272474
Stephan Georg <s.georg@nearest.place>
2017-01-18 15:16:48 +0100

    Added staticmap, marker and zoom

--- test/staticmaps.js
+++ test/staticmaps.js
@@ -7,13 +7,73 @@ describe('StaticMap', function(){
 
   describe('Initializating', function() {
 
+
+
     it('without any arguments', function() {
+
       (function() {
-        map = new StaticMaps(300, 400);
-
-        map.render(10, [13.437524,52.4945528]);
-
+        map = new StaticMaps(500, 500);
       }).should.not.throw();
+    });
+
+
+    it('render', function(done) {
+
+      this.timeout(5000);
+
+      map = new StaticMaps(1200, 800);
+      map.render(13, [13.437524,52.4945528])
+        .then(function(values) {
+          var save = map.image.save( 'simple.png');
+          save.on('finish', function () {
+            done();
+          });
+        })
+        .catch(function(err) { console.log(err); });
+
+    });
+
+    it('render w/ icon', function(done) {
+
+     this.timeout(10000);
+
+      var file = __dirname + '/marker.png';
+
+      map = new StaticMaps(500, 500);
+      map.addMarker([13.437524,52.4945528], file, 24, 48, 48, 48);
+      map.addMarker([13.430524,52.4995528], file, 24, 48, 48, 48);
+
+      map.render(12, [13.437524,52.4945528])
+        .then(function(values) {
+          var save = map.image.save('marker.png');
+          save.on('finish', function () {
+            done();
+          });
+        })
+        .catch(function(err) { console.log(err); });
+
+    });
+
+    it('render w/out center', function(done) {
+
+     this.timeout(10000);
+
+      var file = __dirname + '/marker.png';
+
+      map = new StaticMaps(1000, 1000);
+      map.addMarker([13.437524,52.4945528], file, 24, 48, 48, 48);
+      map.addMarker([13.430524,52.4995528], file, 24, 48, 48, 48);
+      map.addMarker([13.410524,52.5195528], file, 24, 48, 48, 48);
+
+      map.render()
+        .then(function(values) {
+          var save = map.image.save('marker-nocenter.png');
+          save.on('finish', function () {
+            done();
+          });
+        })
+        .catch(function(err) { console.log(err); });
+
     });
 
   });
