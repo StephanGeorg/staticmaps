8e93c4feea74ed16a10dedaafb88e1f449b8e0e5
Stephan Georg <s.georg@nearest.place>
2017-10-31 13:25:55 +0100

    Added chai and implemented full es6

--- test/staticmaps.js
+++ test/staticmaps.js
@@ -1,220 +1,162 @@
-var should = require('should');
-var path = require('path');
+import path from 'path';
 
-var StaticMaps = require('../dist/lib/staticmaps');
+const { expect } = require('chai');
 
-var markerPath = path.join(__dirname, 'marker.png');
+const StaticMaps = require('../dist/lib/staticmaps');
 
-describe('StaticMap', function(){
-  var geocoder;
+const markerPath = path.join(__dirname, 'marker.png');
 
-  describe('Initializing ...', function() {
-
-    it('without any arguments', function() {
-
-      (function() {
-
-        var options = {
+describe('StaticMap', () => {
+  describe('Initializing ...', () => {
+    it('without any arguments', () => {
+      expect(() => {
+        const options = {
           width: 600,
-          height: 200
+          height: 200,
         };
-        map = new StaticMaps(options);
-
-      }).should.not.throw();
+        const map = new StaticMaps(options);
+      }).to.not.throw();
     });
-
   });
 
-
-  describe('Rendering ...', function() {
-
-    it('render w/ center', function(done) {
-
-      this.timeout(3000);
-
-      var options = {
+  describe('Rendering ...', () => {
+    it('render w/ center', (done) => {
+      const options = {
         width: 600,
-        height: 200
+        height: 200,
       };
-
-      map = new StaticMaps(options);
-      map.render([13.437524,52.4945528], 13)
-        .then(function() {
-          return map.image.save('test/out/01-center.jpg');
-        })
+      const map = new StaticMaps(options);
+      map.render([13.437524, 52.4945528], 13)
+        .then(() => map.image.save('test/out/01-center.jpg'))
         .then(done)
         .catch(done);
+    }).timeout(3000);
 
-    });
-
-    it('render w/ center from custom', function(done) {
-
-      this.timeout(3000);
-
-      var options = {
+    it('render w/ center from custom', (done) => {
+      const options = {
         width: 600,
         height: 200,
       };
 
-      map = new StaticMaps(options);
-      map.render([13.437524,52.4945528], 13)
-        .then(function () {
-          return map.image.save('test/out/02-center_osm.png');
-        })
+      const map = new StaticMaps(options);
+      map.render([13.437524, 52.4945528], 13)
+        .then(() => map.image.save('test/out/02-center_osm.png'))
         .then(done)
         .catch(done);
+    }).timeout(3000);
 
-    });
-
-    it('render w/ bbox', function(done) {
-
-      this.timeout(3000);
-
-      var options = {
+    it('render w/ bbox', (done) => {
+      const options = {
         width: 1200,
-        height: 800
+        height: 800,
       };
 
-      map = new StaticMaps(options);
-      map.render([11.414795,51.835778, 11.645164,51.733833])
-        .then(function() {
-          return map.image.save('test/out/03-bbox.png');
-        })
+      const map = new StaticMaps(options);
+      map.render([11.414795, 51.835778, 11.645164, 51.733833])
+        .then(() => map.image.save('test/out/03-bbox.png'))
         .then(done)
         .catch(done);
+    }).timeout(3000);
 
-    });
-
-    it('render w/ icon', function(done) {
-
-     this.timeout(3000);
-
-      var options = {
+    it('render w/ icon', (done) => {
+      const options = {
         width: 500,
-        height: 500
+        height: 500,
       };
 
-      map = new StaticMaps(options);
+      const map = new StaticMaps(options);
 
-      var marker = {
+      const marker = {
         img: markerPath,
         offsetX: 24,
         offsetY: 48,
         width: 48,
-        height: 48
+        height: 48,
       };
 
-      marker.coord = [13.437524,52.4945528];
+      marker.coord = [13.437524, 52.4945528];
       map.addMarker(marker);
 
-      marker.coord = [13.430524,52.4995528];
+      marker.coord = [13.430524, 52.4995528];
       map.addMarker(marker);
 
-      map.render([13.437524,52.4945528], 12)
-        .then(function () {
-          return map.image.save('test/out/04-marker.png');
-        })
+      map.render([13.437524, 52.4945528], 12)
+        .then(() => map.image.save('test/out/04-marker.png'))
         .then(done)
         .catch(done);
+    }).timeout(3000);
 
-    });
-
-    it('render w/out center', function(done) {
-
-     this.timeout(3000);
-
-      var options = {
+    it('render w/out center', (done) => {
+      const options = {
         width: 1200,
-        height: 800
+        height: 800,
       };
-      map = new StaticMaps(options);
-
-      var marker = {
+      const map = new StaticMaps(options);
+      const marker = {
         img: markerPath,
         offsetX: 24,
         offsetY: 48,
         width: 48,
-        height: 48
+        height: 48,
       };
 
-      marker.coord = [13.437524,52.4945528];
+      marker.coord = [13.437524, 52.4945528];
       map.addMarker(marker);
-      marker.coord = [13.430524,52.4995528];
+      marker.coord = [13.430524, 52.4995528];
       map.addMarker(marker);
-      marker.coord = [13.410524,52.5195528];
+      marker.coord = [13.410524, 52.5195528];
       map.addMarker(marker);
 
       map.render()
-        .then(function() {
-          return map.image.save('test/out/05-marker-nocenter.png');
-        })
+        .then(() => map.image.save('test/out/05-marker-nocenter.png'))
         .then(done)
         .catch(done);
-
-    });
-
+    }).timeout(3000);
   });
 
-  describe('Rendering w/ lines ...', function() {
-
-    it('render w/ single line', function(done) {
-
-      this.timeout(5000);
-
-      var options = {
+  describe('Rendering w/ lines ...', () => {
+    it('render w/ single line', (done) => {
+      const options = {
         width: 600,
         height: 300,
         paddingX: 50,
-        paddingY: 50
+        paddingY: 50,
       };
 
-      map = new StaticMaps(options);
+      const map = new StaticMaps(options);
 
-      var line = {
+      const line = {
         coords: [
-          [13.399259,52.482659],
-          [13.387849,52.477144],
-          [13.40538,52.510632]
+          [13.399259, 52.482659],
+          [13.387849, 52.477144],
+          [13.40538, 52.510632],
         ],
         color: '#0000FFBB',
-        width: 3
+        width: 3,
       };
 
       map.addLine(line);
-
       map.render()
-        .then(function(values) {
-          return map.image.save('test/out/06-line.png');
-        })
+        .then(() => map.image.save('test/out/06-line.png'))
         .then(done)
         .catch(done);
-
-    });
+    }).timeout(5000);
   });
 
-  describe('Rendering buffer ...', function() {
-
-    it('render w/ center', function(done) {
-
-      this.timeout(3000);
-
-      var options = {
+  describe('Rendering buffer ...', () => {
+    it('render w/ center', (done) => {
+      const options = {
         width: 600,
-        height: 200
+        height: 200,
       };
 
-      map = new StaticMaps(options);
-      map.render([13.437524,52.4945528], 13)
-        .then(function() {
-          map.image.buffer('image/png');
-        })
-        .then(function (buffer) {
+      const map = new StaticMaps(options);
+      map.render([13.437524, 52.4945528], 13)
+        .then(() => map.image.buffer('image/png'))
+        .then((buffer) => {
           done();
         })
         .catch(done);
-
-    });
+    }).timeout(3000);
   });
-
-
 });
