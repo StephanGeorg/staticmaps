0569d6109c610524062322bb27a98839bf630cec
Gihan S <gihanshp+github@gmail.com>
2018-10-09 15:20:02 +0530

    Added Math.round to avoid validation errors in Sharp.

--- src/staticmaps.js
+++ src/staticmaps.js
@@ -288,7 +288,7 @@ class StaticMaps {
       this.markers.forEach((marker) => {
         queue.push(async () => {
           this.image.image = await sharp(this.image.image)
-            .overlayWith(marker.imgData, { top: marker.position[1], left: marker.position[0] })
+            .overlayWith(marker.imgData, { top: Math.round(marker.position[1]), left: Math.round(marker.position[0]) })
             .toBuffer();
         });
       });
